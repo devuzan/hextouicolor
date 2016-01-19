a514ba8f21135b874c17185ee63dda0a1b4c944c
Yusuf U <hx0a1q@Yusuf-MacBook-Pro.local>
2016-01-19 09:48:47 +0200

    First commit

---
+++ HextoUIColor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HextoUIColor
+//
+//  Created by Yusuf U. on 16/01/16.
+//  Copyright © 2016 Yusuf U. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HextoUIColor/MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  HextoUIColor
+//
+//  Created by Yusuf U. on 16/01/16.
+//  Copyright © 2016 Yusuf U. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
